021d0b8c7282c57a4392d72adca15603b24cfae9
Igor Gabriel <147875152+Igorgmom@users.noreply.github.com>
2026-03-23 15:28:38 -0300

    Update client.py to handle multiple server requests
    Refactor client.py to send multiple messages to the server and print responses.

--- client.py
+++ client.py
@@ -1,9 +1,20 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
-data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
-s.close()               # close the connection
+testes = [
+    "ASTERISCO:Olá mundo",
+    "MAIUSCULO:estou gritando no servidor"
+]
+
+for msg in testes:
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect((HOST, PORT))
+    
+    print(f"Enviando requisição: {msg}")
+    s.send(str.encode(msg)) 
+    
+    data = s.recv(1024) 
+    print(f"Resposta do servidor: {bytes.decode(data)}")
+    
+    s.close()
+    print("-" * 30)
